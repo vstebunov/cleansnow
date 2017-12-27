499505b3eca5a7b8e9dff50dae64075feb98bf28
Vladimir Stebunov <vladimir_stebunov@epam.com>
2017-12-27 15:59:22 +0300

    add simple events

--- main.lua
+++ main.lua
@@ -15,9 +15,34 @@ local width=print(string,0,-6)
 local x = (240-width)//2
 local y = (136-6)//2
 
-function TIC() 
+local events = []
+local btnLabel={"Up","Down","Left","Right","Btn A","Btn B"}
+
+function readkeyboard() 
+    for key = 0, 5 do
+        if btn(key) then
+            table.insert(events, btnLabel[key + 1] .. " pressed")
+        end
+    end
+end
+
+function showevents()
+    for _, event in pairs(events) do
+        print(event, 10, 10 + i * 10)
+    end
+end
+
+function draw()
     cls(0);
     map(0, 0, 5, 5, 30, 30);
-    spr(1, 32, 32);
+    spr(1, 32, 32, 0);
     print(string, x, y)
+end
+
+function TIC() 
+    readkeyboard()
+    draw()
+    showevents()
+    -- event done
+    table.remove(event, 1);
 end
